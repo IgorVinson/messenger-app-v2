829e7f2c519effa3df567d06d2c9189a6cf6c91e
igorv <igorvinson@gmail.com>
2023-02-09 23:36:28 -0500

    add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import {Inter} from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import {useSelector} from "react-redux";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
 
-  const users = useSelector((state: any) => state.users)
+    const users = useSelector((state: any) => state.users)
 
-  console.log(users)
-  return (
-    <>
-
-    </>
-  )
+    console.log(users)
+    return (
+        <>
+            <h1>I am messenger !!!</h1>
+        </>
+    )
 }
