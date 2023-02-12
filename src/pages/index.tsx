61b6433a3a4c79a328f24cdef68ecd87796b9b4a
IgorVinson <igorvinson@gmail.com>
2023-02-11 23:17:00 -0500

    add fetch Contacts and display them on a screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
-import {useSelector} from "react-redux";
 import MainLayOut from "@/components/layouts/MainLayOut";
 
 
 export default function Home() {
 
-    const users = useSelector((state: any) => state.users)
-
-    console.log(users)
     return (
         <>
             <MainLayOut/>
+
         </>
     )
 }
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,14 +1,13 @@
 import { createReducer} from "@reduxjs/toolkit";
 import {State} from "@/types/State";
 
-
 const initialState: State = {
     contacts: []
 };
 
 const rootReducer = createReducer(initialState, {
     'contacts/getContacts': (state, action) => {
-        state.contacts = [...state.contacts, ...action.payload]
+        state.contacts = [...action.payload]
     }
 });
 export default rootReducer;
